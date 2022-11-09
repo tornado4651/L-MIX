968e414c4ac7e293d9ae455f8120370154ff3b22
tornado4651 <tornado4651@163.com>
2022-11-09 12:33:42 +0800

    前后端：初始化框架导入

---
+++ l-mix-boot/src/main/java/com/tornado4651/lmix/boot/LMixBootApplication.java
@@ -0,0 +1,12 @@
+package com.tornado4651.lmix.boot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LMixBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LMixBootApplication.class, args);
+    }
+}
