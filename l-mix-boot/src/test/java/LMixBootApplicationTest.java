b67c4b9bf15d12f9998ed21bc586c1a4f8ecc24c
tornado4651 <tornado4651@163.com>
2022-11-22 11:16:55 +0800

    后端：简化spring-boot-starter-redis依赖并修改测试

--- l-mix-boot/src/test/java/LMixBootApplicationTest.java
+++ l-mix-boot/src/test/java/LMixBootApplicationTest.java
@@ -9,6 +9,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 
 import java.text.ParseException;
 import java.util.List;
+import java.util.Set;
 
 
 @SpringBootTest(classes = {LMixBootApplication.class})
@@ -22,19 +23,6 @@ public class LMixBootApplicationTest {
      */
     @Test
     public void testMyBatis() {
-//        userMapper.delete(new QueryWrapper<User>().eq("username","admin"));
-//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM");
-//        User user = User.builder()
-//                .nickname("管理员")
-//                .username("admin")
-//                .password("123456")
-//                .telephone("13888888888")
-//                .gender((byte) 1)
-//                .avatar(null)
-//                .birthday(dateFormat.parse("1996-08"))
-//                .build();
-//        userMapper.insert(user);
-//        System.out.println("插入完毕！");
         List<User> userList = userMapper.selectList(new LambdaQueryWrapper<>());
         System.out.println(userList);
     }
@@ -46,5 +34,7 @@ public class LMixBootApplicationTest {
     public void testRedis(){
         Object ttttt = redisTemplate.opsForValue().get("ttttt");
         System.out.println(ttttt);
+        Set<String> keys = redisTemplate.keys("*");
+        System.out.println(keys);
     }
 }
