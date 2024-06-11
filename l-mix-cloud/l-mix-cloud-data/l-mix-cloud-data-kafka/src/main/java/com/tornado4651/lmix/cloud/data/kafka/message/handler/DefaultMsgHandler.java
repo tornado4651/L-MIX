989604c3b08728dac25b52114bbe3b885dbecfe1
tornado4651 <tornado4651@163.com>
2024-06-11 17:25:59 +0800

    后端：新增lmix-cloud-data-kafka模块

---
+++ l-mix-cloud/l-mix-cloud-data/l-mix-cloud-data-kafka/src/main/java/com/tornado4651/lmix/cloud/data/kafka/message/handler/DefaultMsgHandler.java
@@ -0,0 +1,27 @@
+package com.tornado4651.lmix.cloud.data.kafka.message.handler;
+
+import org.apache.kafka.common.protocol.Message;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author tornado4651
+ * @date 2024/6/7 17:48
+ */
+@Component
+public class DefaultMsgHandler implements MsgHandler<String> {
+
+    @Override
+    public String convert(Message message) {
+        return null;
+    }
+
+    @Override
+    public void validateMsg(String topic) {
+
+    }
+
+    @Override
+    public void handle(String message) {
+
+    }
+}
---
+++ l-mix-cloud/l-mix-cloud-data/l-mix-cloud-data-kafka/src/main/java/com/tornado4651/lmix/cloud/data/kafka/message/handler/MsgHandler.java
@@ -0,0 +1,29 @@
+package com.tornado4651.lmix.cloud.data.kafka.message.handler;
+
+import org.apache.kafka.common.protocol.Message;
+
+/**
+ * L-MIX topic处理器
+ * @author tornado4651
+ * @date 2024/6/7 15:17
+ */
+public interface MsgHandler<T> {
+
+    T convert(Message message);
+
+
+    void validateMsg(T topic);
+
+
+    void handle(T message);
+
+
+    default void hadleMessage(Message message) {
+
+        T contertedMessage = convert(message);
+
+        validateMsg(contertedMessage);
+
+        handle(contertedMessage);
+    }
+}
