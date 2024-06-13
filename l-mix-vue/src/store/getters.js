747f069671c2f45cdf00e1f566fc2576d329d704
tornado4651 <tornado4651@163.com>
2024-06-13 14:20:30 +0800

    项目结构修改：移除l-mix-vue部分，新增项目“l-mix-vue”

--- l-mix-vue/src/store/getters.js
+++
@@ -1,9 +0,0 @@
-const getters = {
-  sidebar: state => state.app.sidebar,
-  device: state => state.app.device,
-  token: state => state.user.token,
-  avatar: state => state.user.avatar,
-  user: state => state.user
-  // name: state => state.user.name
-}
-export default getters
