f83907f15a8047fb71e7d88031ee32a2311cadbb
tornado4651 <tornado4651@163.com>
2024-06-13 15:06:55 +0800

    项目结构修改：将l-mix-cloud-common、l-mix-cloud-data移出l-mix-cloud项目，作为通用模块与l-mix-cloud、l-mix-boot同级管理

---
+++ l-mix-cloud/l-mix-cloud-auth/src/main/java/com/tornado4651/lmix/cloud/auth/holder/LoginUserHolder.java
@@ -0,0 +1,34 @@
+package com.tornado4651.lmix.cloud.auth.holder;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.alibaba.fastjson2.JSONObject;
+import com.tornado4651.lmix.cloud.common.constants.AuthConstant;
+import com.tornado4651.lmix.cloud.common.dto.UserDTO;
+import com.tornado4651.lmix.cloud.common.exception.ErrorException;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+/**
+ * 获取登录用户信息
+ */
+public class LoginUserHolder {
+
+    public static UserDTO getCurrentUser(){
+        //从Header中获取用户信息
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = servletRequestAttributes.getRequest();
+        String loginUserInfoStr = request.getHeader(AuthConstant.AUTHORIZATION_INFO_HEADER);
+        if (loginUserInfoStr == null) {
+            throw new ErrorException("登录用户信息解析错误！");
+        }
+        String loginUserInfoJsonStr = new String(Base64.getDecoder().decode(loginUserInfoStr), StandardCharsets.UTF_8);
+        JSONObject loginUserInfo = JSONObject.parseObject(loginUserInfoJsonStr);
+        UserDTO userDTO = new UserDTO();
+        BeanUtil.copyProperties(loginUserInfo, userDTO);
+        return userDTO;
+    }
+}
