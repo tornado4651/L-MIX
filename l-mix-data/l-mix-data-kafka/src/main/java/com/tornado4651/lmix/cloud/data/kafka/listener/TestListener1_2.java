d260c37e79aa4265dc7757494ab8a6a57092d06b
tornado4651 <tornado4651@163.com>
2024-06-13 15:06:49 +0800

    项目结构修改：将l-mix-cloud-common、l-mix-cloud-data移出l-mix-cloud项目，作为通用模块与l-mix-cloud、l-mix-boot同级管理

---
+++ l-mix-data/l-mix-data-kafka/src/main/java/com/tornado4651/lmix/cloud/data/kafka/listener/TestListener1_2.java
@@ -0,0 +1,18 @@
+package com.tornado4651.lmix.cloud.data.kafka.listener;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author tornado4651
+ * @date 2024/6/11 14:55
+ */
+@Component
+public class TestListener1_2 {
+
+    @KafkaListener(topics = {"test-topic"}, id="test_listener_1_2", groupId = "test_group_1")
+    public void listen1(String msg) {
+        System.out.println("test_listener_1_2: " + msg);
+    }
+
+}
---
+++ l-mix-data/l-mix-data-kafka/src/main/java/com/tornado4651/lmix/cloud/data/kafka/listener/TestListener2_1.java
@@ -0,0 +1,18 @@
+package com.tornado4651.lmix.cloud.data.kafka.listener;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author tornado4651
+ * @date 2024/6/11 14:55
+ */
+@Component
+public class TestListener2_1 {
+
+    @KafkaListener(topics = {"test-topic"}, id="test_listener_2_1", groupId = "test_group_2")
+    public void listen1(String msg) {
+        System.out.println("test_listener_2_1: " + msg);
+    }
+
+}
